7b1dc950150f08b6d1bd349a60a33d35ebd99e16
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-12 12:02:38 -0400

    añadir titulo

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,6 +1,15 @@
 import streamlit as st 
 
-st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
+st.header.selectbox('Lenguage',[EN,ES])
+
+c1, c2, c3 = st.columns([1, 3, 3])
+c1.image('imagenes/avatar', width=45)
+c3.text(['imagenes/github'](https://sebastiandres.xyz))
+c3.text(['imagenes/linkedin']('https://www.linkedin.com/in/iveliz-alexandra-ayala-lopez-79b2691b1/)')
+c3.text(['imagenes/twitter_4'](https://sebastiandres.xyz))
+
+st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
+
 
  
      
