c638a5c6ea03f6c1ca1fc80cfa108c822a16c28f
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-12 12:06:19 -0400

    arreglar titulo

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -6,7 +6,7 @@ c1, c2, c3 = st.columns([1, 3, 3])
 c1.image('imagenes/avatar.jpg', width=45)
 c2.text('Iveliz A')
 c3.text(['imagenes/github.png'](https://sebastiandres.xyz))
-c3.text(['imagenes/linkedin.png']('https://www.linkedin.com/in/iveliz-alexandra-ayala-lopez-79b2691b1/)')
+c3.text(['imagenes/linkedin.png']('https://www.linkedin.com/in/iveliz-alexandra-ayala-lopez-79b2691b1/)'))
 c3.text(['imagenes/twitter_4.png'](https://sebastiandres.xyz))
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
