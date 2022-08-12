2fc3448d32c155dde47e870cbe22ccbb3084fde3
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-11 20:11:23 -0400

    añadiendo texto

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 
 st.write("Hello from the mojix")
+
+def my_decorator_name(name):
+    def my_custome_decorator(function):
+        def wrapper(*args, **kwargs):
+
+            print('Name:', name)
+            return function(*args, **kwargs)
+
+        return wrapper
+
+    return my_custome_decorator
+
+@my_decorator_name('CodigoFácilito')
+def suma(a, b):
+    return a + b
