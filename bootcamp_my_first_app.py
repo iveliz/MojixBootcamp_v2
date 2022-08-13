2d3b170b03a7c52eaaf7c60b2cfadd2acda58e85
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-13 12:16:33 -0400

    corregir errores

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,20 +3,19 @@ import streamlit as st
 
 
 c1, c2, c3 = st.columns([1, 3, 3])
-c1.image('imagenes/avatar.jpg', width=45, height=45)
+c1.image('imagenes/avatar.jpg', width=30)
 c2.markdown('Iveliz A')
 
 c3.text('imagenes/github.png')
 c3.text('imagenes/linkedin.png')
 c3.text('imagenes/twitter_4.png')
 
-c2.markdown("")
+
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.caption('Simple but effective tips for every python lovers')
 st.image('imagenes/im.jpeg')
 
 
- 
      
 
 
