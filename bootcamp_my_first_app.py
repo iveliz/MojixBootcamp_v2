824a1da5e46d7c1a22716ba7bf5c1cb7c8281c05
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-13 12:34:22 -0400

    Arreglar formato de imagenes

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,17 +2,14 @@ import streamlit as st
 
 
 
-c1, c2, c3 = st.columns([1, 3, 3])
-c1.image('imagenes/avatar.jpg', width=30)
-c2.markdown('Iveliz A')
+c1, c2, c3 = st.columns([3, 3, 3])
+c1.image('imagenes/avatar.jpg', width=50)
+c2.markdown(f"#### @Iveliz A en ![]('imagenes/github.jpg') ![]('imagenes/linkedin.jpg') ![]('imagenes/twitter_4.jpg')")
 
-c3.text('imagenes/github.png')
-c3.text('imagenes/linkedin.png')
-c3.text('imagenes/twitter_4.png')
 
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
-st.caption('Simple but effective tips for every python lovers')
+st.subheader('Simple but effective tips for every python lovers')
 st.image('imagenes/im.jpeg')
 
 
