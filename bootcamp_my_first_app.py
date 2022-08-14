3fa81eff85795f0d22a57def0238a22de3323edc
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-13 21:50:25 -0400

    agregar texto prueba

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -9,7 +9,13 @@ st.image('imagenes/im.jpeg', caption='Photo by Miesha Maiden from Pexels')
 st.markdown('#### The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.')
 st.markdown('#### In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
 
-
-     
+st.header('1. Walrus operator')
+st.text('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+st.subheader('Example')
+st.text('If we want to check and print the length of a list:')
+st.code('Mylist = [1,2,3] if(l := len(mylist) > 2) print(l)')
+st.subheader('Output')
+st.code('3') 
+   
 
 
