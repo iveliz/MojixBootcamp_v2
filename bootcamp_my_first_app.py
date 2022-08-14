8f39e28c619e2e7a1f4fbb9d6f4321141c99f0ed
Iveliz Alexandra Ayala <ayalaiveliz@gmail.com>
2022-08-13 21:22:40 -0400

    arreglar espacios

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -4,7 +4,7 @@ st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 st.subheader('Simple but effective tips for every python lovers')
 st.image('imagenes/im.jpeg', caption='Photo by Miesha Maiden from Pexels')
 
-st.text('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable tim')
+st.text('###### The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
 
      
 
